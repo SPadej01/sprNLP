181cc0b8716d1f22b9b8379b4cdf876f4c6f1131
SPadej01 <slavek.priv@gmail.com>
2023-11-05 16:18:28 +0000

    Zmiany z Colab

---
+++ p_multi_head_attention.py
@@ -0,0 +1,37 @@
+from keras.layers.layer import MultiHeadAttention
+
+class PMultiHeadAttention(MultiHeadAttention):
+  def __init__(
+        self,
+        num_heads,
+        key_dim,
+        value_dim=None,
+        dropout=0.0,
+        use_bias=True,
+        output_shape=None,
+        attention_axes=None,
+        kernel_initializer="glorot_uniform",
+        bias_initializer="zeros",
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        activity_regularizer=None,
+        kernel_constraint=None,
+        bias_constraint=None,
+        **kwargs,
+    ):
+    super().__init__(num_heads=num_heads,
+        key_dim=key_dim,
+        value_dim=value_dim,
+        dropout=dropout,
+        use_bias=use_bias,
+        output_shape=output_shape,
+        attention_axes=attention_axes,
+        kernel_initializer=kernel_initializer,
+        bias_initializer=bias_initializer,
+        kernel_regularizer=kernel_regularizer,
+        bias_regularizer=bias_regularizer,
+        activity_regularizer=activity_regularizer,
+        kernel_constraint=kernel_constraint,
+        bias_constraint=bias_constraint,
+        **kwargs)
+
