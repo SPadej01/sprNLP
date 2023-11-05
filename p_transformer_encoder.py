9d3fc041cdd99f24e8fb48f82bdc2202dd0c6c6e
SPadej01 <slavek.priv@gmail.com>
2023-11-05 20:51:04 +0000

    Zmiany z Colab

--- p_transformer_encoder.py
+++ p_transformer_encoder.py
@@ -5,25 +5,8 @@ from keras_nlp.src.backend import keras
 
 class PTransformerEncoder(TransformerEncoder):
       def __init__(
-        self,
-        intermediate_dim,
-        num_heads,
-        dropout=0,
-        activation="relu",
-        layer_norm_epsilon=1e-05,
-        kernel_initializer="glorot_uniform",
-        bias_initializer="zeros",
-        normalize_first=False,
         **kwargs,    ):
         super().__init__(
-          intermediate_dim=intermediate_dim,
-          num_heads=num_heads,
-          dropout=dropout,
-          activation=activation,
-          layer_norm_epsilon=layer_norm_epsilon,
-          kernel_initializer=kernel_initializer,
-          bias_initializer=bias_initializer,
-          normalize_first=normalize_first,
           **kwargs)
 
       def build(self, inputs_shape):
