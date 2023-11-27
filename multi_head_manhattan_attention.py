a22598037dae3b3b9e99c5cdaddbce7acf1dca24
SPadej01 <slavek.priv@gmail.com>
2023-11-27 15:40:36 +0000

    Zmiany z Colab

---
+++ multi_head_manhattan_attention.py
@@ -0,0 +1,59 @@
+#from keras.layers import MultiHeadAttention
+#Trzeba być zgodnym z Keras 3.0....
+from keras_core.src.layers import MultiHeadAttention
+from keras_core import ops
+import tensorflow as tf
+import numpy as np
+
+
+
+
+class MultiHeadManhattanAttention(MultiHeadAttention):
+  def __init__(
+        self,
+         **kwargs,
+    ):
+
+
+
+    super().__init__(
+        **kwargs)
+
+
+def _compute_attention(
+    self, query, key, value, attention_mask=None, training=None
+):
+    """Applies Manhattan Distance Similarity attention with query, key, value tensors.
+
+    Args:
+        query: Projected query tensor of shape `(B, T, N, key_dim)`.
+        key: Projected key tensor of shape `(B, S, N, key_dim)`.
+        value: Projected value tensor of shape `(B, S, N, value_dim)`.
+        attention_mask: a boolean mask of shape `(B, T, S)`, that prevents
+            attention to certain positions. It is generally not needed if
+            the `query` and `value` (and/or `key`) are masked.
+        training: Python boolean indicating whether the layer should behave
+            in training mode (adding dropout) or in inference mode (no dropout).
+
+    Returns:
+      attention_output: Multi-headed outputs of attention computation.
+      attention_scores: Multi-headed attention weights.
+    """
+    # Compute the Manhattan distance between query and key tensors.
+    attention_scores = tf.reduce_sum(tf.abs(query[:, :, :, :, None] - key[:, :, :, None, :, :]), axis=-1)
+
+    attention_scores = self._masked_softmax(
+        attention_scores, attention_mask
+    )
+
+    # This is actually dropping out entire tokens to attend to, which might
+    # seem a bit unusual, but is taken from the original Transformer paper.
+    attention_scores_dropout = self._dropout_layer(
+        attention_scores, training=training
+    )
+
+    # `context_layer` = [B, T, N, H]
+    attention_output = ops.einsum(
+        self._combine_equation, attention_scores_dropout, value
+    )
+    return attention_output, attention_scores
--- p_transformer_encoder.py
+++ p_transformer_encoder.py
@@ -2,6 +2,7 @@ from keras_nlp.layers import TransformerEncoder
 from multi_head_cosine_attention import MultiHeadCosineAttention
 from multi_head_euclidean_attention import MultiHeadEuclideanAttention
 from multi_head_smoothed_cosine_attention import MultiHeadSmoothedCosineAttention
+from multi_head_manhattan_attention import MultiHeadManhattanAttention
 from keras_nlp.src.utils.keras_utils import clone_initializer
 from keras_nlp.src.backend import keras
 
@@ -36,6 +37,10 @@ class PTransformerEncoder(TransformerEncoder):
           attention_class=MultiHeadEuclideanAttention  # atencja euklidesowa
         elif self.attention_type=="SmoothedCosine":
           attention_class=MultiHeadSmoothedCosineAttention  # atencja euklidesowa
+        elif self.attention_type=="Manhattan":
+          attention_class=MultiHeadManhattanAttention  # atencja euklidesowa
+
+
         else:
           raise ValueError(f"Nie wyznaczono klasy atencji dla parametru: {self.attention_type}")
 
